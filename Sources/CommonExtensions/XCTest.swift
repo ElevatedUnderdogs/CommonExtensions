dbc5b0a1e897caed042b6c77f29e150da996f979
S <scottlydon18@gmail.com>
2022-05-14 17:59:12 -0700

    Remove XCTAssertOrPrint because it doesn't like that framework.

--- Sources/CommonExtensions/XCTest.swift
+++ Sources/CommonExtensions/XCTest.swift
@@ -4,16 +4,16 @@
 //
 //  Created by Scott Lydon on 5/14/22.
 //
-
-import Foundation
-import XCTest
-
-public extension XCTest {
-    func XCTAssertOrPrint(_ assertion: Bool, _ message: String) {
-        if assertion {
-            print(message)
-        } else {
-            XCTAssert(assertion, message)
-        }
-    }
-}
+//
+//import Foundation
+//import XCTest
+//
+//public extension XCTest {
+//    func XCTAssertOrPrint(_ assertion: Bool, _ message: String) {
+//        if assertion {
+//            print(message)
+//        } else {
+//            XCTAssert(assertion, message)
+//        }
+//    }
+//}
