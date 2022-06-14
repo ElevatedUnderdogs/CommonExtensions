9039e26466f956a387844525df01c220f9ae5f33
S <scottlydon18@gmail.com>
2022-06-13 19:17:10 -0700

    Add APIHelper methods.

--- Sources/CommonExtensions/String.swift
+++ Sources/CommonExtensions/String.swift
@@ -29,6 +29,15 @@ public extension String {
         }
         return ""
     }
+
+    var deterministicHash: UInt64 {
+        var result = UInt64 (5381)
+        let buf = [UInt8](utf8)
+        for b in buf {
+            result = 127 * (result & 0x00ffffffffffffff) + UInt64(b)
+        }
+        return result
+    }
 //
 //    var path: String {
 //        return substring(from: UserDefaults.baseUrlToUse, to: ".php?")
--- Sources/CommonExtensions/URLRequest.swift
+++ Sources/CommonExtensions/URLRequest.swift
@@ -32,3 +32,9 @@ import Foundation
 //    }
 //}
 
+extension URLRequest {
+
+    var deterministicHash: String {
+        String((url.string + httpMethod.string + (httpBody?.string ?? "")).deterministicHash)
+    }
+}
