f477da64cfd255d824663f222d2b6a1b2d66debb
S <scottlydon18@gmail.com>
2022-06-19 00:16:09 -0700

    Add desktop url check.

--- Sources/CommonExtensions/Bundle.swift
+++ Sources/CommonExtensions/Bundle.swift
@@ -83,7 +83,7 @@ public extension URL {
     /// Creates a file and writes to it.  If the file already exists at this location, then this writes to it
     /// - Parameter text: text to write to the file.
     func createFileAndWrite(text: String) throws {
-        if #available(macOS 10.6, *) {
+        if #available(macOS 10.6, *), absoluteString.contains("desktop") {
             assert(!Bundle.main.ob_isSandboxed, "Delete the sandbox key value pair in the entitlements")
         }
         try text.write(to: self, atomically: true, encoding: .utf8)
